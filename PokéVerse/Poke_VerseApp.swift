74724f612671470103d34021be19a4ca7515b4e9
OscarNengovhela <oscar@zulzi.com>
2024-02-03 07:34:23 +0200

    Initial Commit

---
+++ PokéVerse/Poke_VerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_VerseApp.swift
+//  PokéVerse
+//
+//  Created by Ndamu Nengovhela on 2024/02/03.
+//
+
+import SwiftUI
+
+@main
+struct Poke_VerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
