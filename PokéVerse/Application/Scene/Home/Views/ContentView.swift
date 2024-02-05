634219f8dc16a806d18123236eb2a89ee0d48e29
OscarNengovhela <oscar@zulzi.com>
2024-02-05 14:11:59 +0200

    ContentView: added a no results view

--- PokéVerse/Application/Scene/Home/Views/ContentView.swift
+++ PokéVerse/Application/Scene/Home/Views/ContentView.swift
@@ -15,25 +15,39 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: adaptiveColumuns, spacing: 12) {
-                    ForEach(homeViewModel.filterPokemon) { pokemon in
-                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
-                            PokemonImageView(pokemon: pokemon,
-                                             downloadUrl: homeViewModel.getImageUrl(pokemon: pokemon))
+                if homeViewModel.filterPokemon.isEmpty {
+                    VStack(spacing: 12) {
+                        Image("noResults")
+                            .renderingMode(.template)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 75, height: 75)
+                            .foregroundColor(UIColor.primaryColor)
+                        Text("Sorry we couldn't find any results for your search.")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }.padding(.top, 32)
+                } else {
+                    LazyVGrid(columns: adaptiveColumuns, spacing: 12) {
+                        ForEach(homeViewModel.filterPokemon) { pokemon in
+                            NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
+                                PokemonImageView(pokemon: pokemon,
+                                                 downloadUrl: homeViewModel.getImageUrl(pokemon: pokemon))
+                            }
                         }
+                    }.animation(.smooth(duration: 0.3),
+                                value: homeViewModel.filterPokemon.count)
+                    .navigationTitle("PokéVerse")
+                    .alert(isPresented: $homeViewModel.showAlert) {
+                        Alert(
+                            title: Text("Something went wrong"),
+                            message: Text(homeViewModel.errorMessage),
+                            dismissButton: .default(Text("OK"))
+                        )
                     }
-                }.animation(.smooth(duration: 0.3),
-                            value: homeViewModel.filterPokemon.count)
-                .navigationTitle("PokéVerse")
-                .alert(isPresented: $homeViewModel.showAlert) {
-                    Alert(
-                        title: Text("Something went wrong"),
-                        message: Text(homeViewModel.errorMessage),
-                        dismissButton: .default(Text("OK"))
-                    )
-                }
-                if homeViewModel.isLoading {
-                    ProgressView()
+                    if homeViewModel.isLoading {
+                        ProgressView()
+                    }
                 }
             }.searchable(text: $homeViewModel.searchText)
         }.environmentObject(homeViewModel)
